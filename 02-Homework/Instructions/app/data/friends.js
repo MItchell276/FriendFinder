f2e584ee20e821be06468665111abd6a1301ef87
Dan MItchell <danmitchell@Dans-MacBook-Pro.local>
2019-06-25 12:47:37 -0400

    started

---
+++ 02-Homework/Instructions/app/data/friends.js
@@ -0,0 +1,29 @@
+const friends = [
+    {
+        name: "Jackson",
+        photo: "https://cdn.smehost.net/michaeljacksoncom-uslegacyprod/wp-content/uploads/2019/03/Apr2019Mobile.jpg",
+        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    },
+    {
+        name: "Baby Dee",
+        photo: "https://i.pinimg.com/originals/e8/c3/61/e8c3614e098237d0017b74e4254d236c.jpg",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+    {
+        name: "Charlie",
+        photo: "https://images.vice.com/vice/images/articles/meta/2012/08/10/charlie-murphy-is-angry-at-the-pussification-of-the-world-1413333957866.jpeg?crop=1xw:0.9917355371900827xh;center,center&resize=1200:*",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        name: "Mac",
+        photo: "https://watchtheyard.com/wp-content/uploads/2016/06/MJulien_TheMack_GQ_18Jun12_rex_b-1.jpg",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+        name: "2Pac",
+        photo: "https://images.complex.com/complex/images/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/euixukrnmqea34qmm5yj/tupac",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    }
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,24 @@
+// Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
+
+const express = require("express");
+
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.text());
+
+// The below points our server to a series of "route" files.
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
